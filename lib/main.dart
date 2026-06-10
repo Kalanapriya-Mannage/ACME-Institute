11050c591d324166b4bb0d7197e1f0495f08bc05
Kalanapriya Mannage <kalanapriyamannage@gmail.com>
2026-06-10 21:23:52 +0530

    lib folder updated

---
+++ lib/main.dart
@@ -0,0 +1,97 @@
+import 'package:acme_institute/core/providers/admin_provider.dart';
+import 'package:acme_institute/core/providers/auth_provider.dart';
+import 'package:acme_institute/core/providers/enrollment_provider.dart';
+import 'package:acme_institute/core/providers/student_provider.dart';
+import 'package:acme_institute/core/providers/teacher_provider.dart';
+import 'package:acme_institute/core/router/app_router.dart';
+import 'package:acme_institute/core/theme/app_theme.dart';
+import 'package:acme_institute/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  final authProvider = AuthProvider();
+  await authProvider.loadCurrentUser();
+
+  runApp(MyApp(authProvider: authProvider));
+}
+
+class MyApp extends StatelessWidget {
+  final AuthProvider authProvider;
+  const MyApp({super.key, required this.authProvider});
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider.value(value: authProvider),
+        ChangeNotifierProvider(create: (_) => StudentProvider()),
+        ChangeNotifierProvider(create: (_) => TeacherProvider()),
+        ChangeNotifierProvider(create: (_) => AdminProvider()),
+        ChangeNotifierProvider(create: (_) => EnrollmentProvider()),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'ACME Institute',
+        theme: AppTheme.lightTheme,
+        initialRoute: authProvider.isAuthenticated
+            ? AppRouter.dashboardFor(authProvider.userRole)
+            : AppRouter.roleSelection,
+        onGenerateRoute: AppRouter.onGenerateRoute,
+      ),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: .center,
+          children: [
+            const Text('You have pushed the button this many times:'),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
